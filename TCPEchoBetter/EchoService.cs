7cf2fa1b658e2222a717be010aa0fd29afba71f4
Sebastian Petersen <sebastian@srmail.dk>
2018-09-09 14:38:59 +0200

    Assignment 2-5, done

--- TCPEchoBetter/EchoService.cs
+++ TCPEchoBetter/EchoService.cs
@@ -1,15 +1,74 @@
-namespace DefaultNamespace
+using System;
+using System.IO;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace TCPEchoBetter
 {
     public class EchoService
     {
-        public EchoService()
+        private TcpClient ConnectionSocket { get; }
+
+        public EchoService(TcpClient connectionSocket)
         {
-            DoIt();
+            ConnectionSocket = connectionSocket;
         }
 
         public void DoIt()
         {
+            //var ns = ConnectionSocket.GetStream();
+            Stream ns = new NetworkStream(ConnectionSocket.Client);
             
+            while (true)
+            {
+                if (!ConnectionSocket.Connected)
+                {
+                    Console.WriteLine("No connection to client, restarting server.");
+                    Thread.Sleep(2300);
+                    Console.Clear();
+                    break;
+                }
+                
+                var sr = new StreamReader(ns);
+                var sw = new StreamWriter(ns) {AutoFlush = true};
+                var message = sr.ReadLine();
+                var answer = "";
+                
+                if (message == string.Empty)
+                {
+                    Console.WriteLine("Empty string detected!");
+                    Console.WriteLine("Either the client sent an empty string or the connection is lost.\nRestarting server");
+                    Thread.Sleep(2300);
+                    Console.Clear();
+                    ns.Close();
+                    ConnectionSocket.Close();
+                    break;
+                }
+
+                while (!string.IsNullOrEmpty(message))
+                {
+                    Console.WriteLine("Client: " + message);
+
+                    if (message == "stop")
+                    {
+                        Console.WriteLine("Received interrupt signal!");
+                        Console.WriteLine("The server will close the connection and wait for a new client.");
+                        sw.WriteLine("Received interrupt signal! Closing connection...");
+                        Thread.Sleep(2300);
+                        Console.Clear();
+                        ns.Close();
+                        ConnectionSocket.Close();
+                        break;
+                    }
+
+                    answer = message.ToUpper();
+                    sw.WriteLine(answer);
+                    message = sr.ReadLine();
+                }
+                
+                ns.Close();
+                ConnectionSocket.Close();
+            }
         }
     }
 }
--- TCPEchoBetter/TCPEchoServer.cs
+++ TCPEchoBetter/TCPEchoServer.cs
@@ -1,14 +1,34 @@
 ﻿using System;
+using System.Net;
 using System.Net.Sockets;
 
 namespace TCPEchoBetter
 {
-    class Program
+    class TCPEchoServer
     {
         static void Main(string[] args)
         {
-            EchoService es = new EchoService(new TcpClient("localhost", 6789));
-            es.DoIt();
+            IPAddress ip = IPAddress.Parse("127.0.0.1");
+
+           
+            TcpListener serverSocket = new TcpListener(ip, 6789);
+            //Alternatively but deprecated
+            //TcpListener serverSocket = new TcpListener(6789);
+
+
+            serverSocket.Start();
+            Console.WriteLine("Server started\n");
+
+            while (true)
+            {
+                Console.WriteLine("Waiting for a client to connect...");
+                var connectionSocket = serverSocket.AcceptTcpClient();
+                //Socket connectionSocket = serverSocket.AcceptSocket();
+                Console.WriteLine("Server activated\n");
+            
+                EchoService es = new EchoService(connectionSocket);
+                es.DoIt();
+            }
         }
     }
 }
