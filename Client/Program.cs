b5e17423ac72b30e3cdf0a2b303e5c7b34593a9b
Sebastian Petersen <sebastian@srmail.dk>
2018-09-10 19:19:11 +0200

    Start on socket concurrent assignment. Updated server and client.

--- Client/Program.cs
+++ Client/Program.cs
@@ -15,6 +15,8 @@ using System.Linq;
 using System.Net.Sockets;
 using System.Text;
 using System.Threading.Tasks;
+using System.Diagnostics;
+using System.Threading;
 
 namespace Client
 {
@@ -22,8 +24,25 @@ namespace Client
     {
         static void Main(string[] args)
         {
-            //Console.ReadLine();
-            TcpClient clientSocket = new TcpClient("127.0.0.1", 6789);
+            // Create a new TCPClient
+            TcpClient clientSocket = new TcpClient();
+            try
+            {
+                clientSocket = new TcpClient("127.0.0.1", 6789);
+            }
+            catch (Exception e)
+            {
+                
+                Debug.WriteLine("Der opstod en fejl ved forsøget på at oprette en forbinelse\n" + e + "\n");
+
+                System.Console.WriteLine("Det lader til at der er et problem med forbindelsen til serveren!");
+                System.Console.WriteLine("Prgrammet sover i 5 sekunder og prøver at oprette forbindelse igen.");
+
+                Thread.Sleep(5000);
+                Console.Clear();
+                Main(null);
+            }
+            
             Console.WriteLine("Client ready");
 
             Stream ns = clientSocket.GetStream();  //provides a Stream
@@ -31,19 +50,30 @@ namespace Client
             StreamWriter sw = new StreamWriter(ns);
             sw.AutoFlush = true; // enable automatic flushing
 
-            for (int i = 0; i < 5; i++){
-            string message = Console.ReadLine();
-            sw.WriteLine(message);
-            string serverAnswer = sr.ReadLine();
+            for (int i = 0; i < 5; i++)
+            {
+                try
+                {
+                    string message = Console.ReadLine();
+                    sw.WriteLine(message);
+                    string serverAnswer = sr.ReadLine();
 
-            Console.WriteLine("Server: " + serverAnswer);
+                    Console.WriteLine("Server: " + serverAnswer);
+                }
+                catch (IOException)
+                {
+                    Console.WriteLine("Forbindelsen til servenren blev afbrudt uventet.\nLukker ned...");
+                    Thread.Sleep(3000);
+                    ns.Close();
+                    clientSocket.Close();
+                    Environment.Exit(0);
+                }
             }
 
             Console.WriteLine("No more from server. Press Enter");
             Console.ReadLine();
 
             ns.Close();
-
             clientSocket.Close();
 
         }
---
+++ ConcurrentServer/TCPEchoServer.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Net;
+using System.Net.Sockets;
+using System.Threading;
+
+namespace ConcurrentServer
+{
+    class TCPEchoServer
+    {
+        [SuppressMessage("ReSharper", "FunctionNeverReturns")]
+        static void Main(string[] args)
+        {
+            // prepares the server to accept client on localhost at port 6789.
+            var ip = IPAddress.Parse("127.0.0.1");
+            var serverSocket = new TcpListener(ip, 6789);
+
+            // Starts the server
+            serverSocket.Start();
+            Console.WriteLine("Server started\n");
+
+            // Infinite loop to wait for clients and running the echo service.
+            while (true)
+            {
+                Console.WriteLine("Waiting for a client to connect...");
+                var connectionSocket = serverSocket.AcceptTcpClient();
+                Console.WriteLine("Client connected succesfully\n");
+
+                var es = new EchoService(connectionSocket);
+                Thread myThread = new Thread(es.DoIt);
+                myThread.Start();
+            }
+        }
+    }
+}
